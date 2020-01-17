33d727a21beb64ccf083608643832b4646f201e9
saphidev <saphidev@gmail.com>
2020-01-17 11:40:00 +0100

    add star + dancing moon

--- src/DarkSwitch.js
+++ src/DarkSwitch.js
@@ -8,9 +8,11 @@ const DarkSwitch = ({classSwitch,classHandle,onChange}) => {
     classSwitch  = classSwitch || "switch";
     classHandle = classHandle || "handle"
 
+
     return <span className={classSwitch}>
             <input type="checkbox" onChange={onChange}/>
             <span className={classHandle}></span>
+            <span className="stars"></span>
     </span>
 }
 
